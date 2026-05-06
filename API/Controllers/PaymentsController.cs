0cbe2c1819b5adfa4618ddc6b3f3fd7448cde9f3
Qwerier <klarkahmeti@yahoo.com>
2026-05-06 22:41:40 +0200

    Start coding webhooks for Stripe communication after order

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -10,16 +10,15 @@ using API.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PaymentsController(StoreContext db, PaymentsService paymentsService) : ControllerBase
+    public class PaymentsController(StoreContext db, PaymentsService paymentsService
+    , IConfiguration configuration, ILogger<PaymentsController> logger) : ControllerBase
     {
-        public readonly StoreContext db = db;
-
-        public PaymentsService paymentsService = paymentsService;
 
         [Authorize]
         [HttpPost]
@@ -28,7 +27,7 @@ namespace API.Controllers
             string? cookie = Request.Cookies["basketCookieId"];
             Basket? basket = await db.Baskets
                 .GetBasket(cookie);
-  
+
             if (basket == null) return BadRequest("Problem with the basket");
 
             var intent = await paymentsService.CreateOrUpdatePaymentIntent(basket);
@@ -47,8 +46,54 @@ namespace API.Controllers
 
             //     if(!result) return BadRequest("Problem updating basket");
             // }
- 
+
             return basket.ToDto();
+        }
+
+        public async Task<IActionResult> StripeWebhook()
+        {
+            string json = await new StreamReader(Request.Body).ReadToEndAsync();
+
+            try
+            {
+                var stripeEvent = ConstructStripeEvent(json);
+
+                if (stripeEvent.Data.Object is not PaymentIntent intent)
+                {
+                    return BadRequest("Invalid event data");
+                }
+
+                if (intent.Status == "succeeded") await HandleSuccess(intent);
+                else await HandleFailure(intent);
+
+                return Ok();
+            }
+            catch (StripeException stripeEx)
+            {
+                logger.LogError(stripeEx, "Stripe hook error");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Webhook error");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An unexpected error has occurred");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Webhook error");
+            }
+        }
+
+        private Event ConstructStripeEvent(string json)
+        {
+            try
+            {
+                return EventUtility.ConstructEvent(json
+                , Request.Headers["Stripe-Signature"]
+                , configuration["Stripe:WhSecret"]
+                );
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to construct Stripe webhook event");
+                throw new StripeException("Invalid signature");
+            }
         }
     }
 }
--- frontend/src/app/lib/util.ts
+++ frontend/src/app/lib/util.ts
@@ -1,3 +1,45 @@
+import { ConfirmationToken } from "@stripe/stripe-js";
+import { Order, ShippingAddress } from "../models/Order";
+
 export function currencyFormat(num: number) {
     return '$' + num.toFixed(2);
 }
+
+function isConfirmationToken(obj: any): obj is ConfirmationToken {
+    return obj && typeof obj === 'object' && 'shipping' in obj;
+}
+
+function isOrder(obj: any): obj is Order {
+    return obj && typeof obj === 'object' && 'shippingAddress' in obj;
+}
+
+export function formatAddress(owner: Order | ConfirmationToken | null) {
+    if(!owner) return '';
+
+    if (isConfirmationToken(owner)) {
+        if (!owner.shipping) return '';
+        const { name, address } = owner.shipping;
+        return `${name}, ${address?.line1}, ${address?.city}, ${address?.state},
+        ${address?.postal_code}, ${address?.country}`
+    }
+    else if(isOrder(owner)){
+        const address = owner.shippingAddress;
+
+        return `${address?.name}, ${address?.line1}, ${address?.city}, ${address?.state},
+        ${address?.postal_code}, ${address?.country}`
+    }
+}
+
+//   const paymentString = () => {
+//     const card = order.paymentSummary;
+
+//     return `${card?.brand?.toUpperCase()}, **** **** **** ${card?.last4}, 
+//             Exp: ${card?.exp_month}/${card?.exp_year}`
+//   }
+    // const paymentFormatting = () => {
+    //     if(!confirmationToken?.payment_method_preview.card) return '';
+    //     const {card} = confirmationToken.payment_method_preview;
+    //     return `${card.brand.toUpperCase()}, **** **** **** ${card.last4}, 
+    //         Exp: ${card.exp_month}/${card.exp_year}`
+    // }
+// export function formatPayment(owner:)
